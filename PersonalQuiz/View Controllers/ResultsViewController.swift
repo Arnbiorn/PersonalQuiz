b0494819bb5c9dbc70e1edccafae8ebcc91c1f8f
Arnbiorn <skrutelev988@gmail.com>
2020-10-26 22:52:09 +0300

    Create View Controllers for Introduction View, Questions View, Answers View

---
+++ PersonalQuiz/View Controllers/ResultsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ResultsViewController.swift
+//  PersonalQuiz
+//
+//  Created by Егор Скрутелев on 26.10.2020.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    // 1. Передать сюда массив с ответами
+    // 2. Определить наиболее часто встречающийся тип животного
+    // 3. отобразить результаты на экране
+    // 4. Избавиться от кнопки возврата на предыдущий экран
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
